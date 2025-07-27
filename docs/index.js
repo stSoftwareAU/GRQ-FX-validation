a2485edf59cdd8777434511319672025feb65a07
Nigel Leck <nigel@stSoftware.com.au>
2025-07-28 09:00:51 +1000

    Test updated pre-commit hook with external JS file

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 // Version constant - this will be updated by the git hook
-const VERSION = "1.0.3";
+const VERSION = "1.0.4";
 
 // Set page title with version
 document.title = `GRQ FX Validation Dashboard v${VERSION}`;
@@ -819,3 +819,4 @@ let fxValidator;
 document.addEventListener('DOMContentLoaded', () => {
   fxValidator = new GRQFXValidator();
 }); // Test comment
+// Test comment
