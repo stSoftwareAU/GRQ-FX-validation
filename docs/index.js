551fff2056b674f4cf289fed858861be47d5e560
Nigel Leck <nigel@stSoftware.com.au>
2025-07-28 12:25:26 +1000

    Add FX predictions for 2025-07-28

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 // Version constant - this will be updated by the git hook
-const VERSION = "1.0.21";
+const VERSION = "1.0.22";
 
 // Set page title with version
 document.title = `GRQ FX Validation Dashboard v${VERSION}`;
